d5a061b4b58bae0f9e001cafc48dd2c797b2a58e
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 11:36:55 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+ import java.util.Scanner; 
+
 /**
- *
- * @author 
+ * This program determines the maximum number between 3 numbers
+ * @author Zach Ramirez  
  */
 public class Main {
 
@@ -9,7 +11,31 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //Create a scanner for user input 
+    Scanner input = new Scanner(System.in);  
+
+    //Ask the user for their numbers 
+    System.out.println("please enter your first number"); 
+    int number = input.nextInt(); 
+    System.out.println("please enter your second number"); 
+    int secondNumber = input.nextInt(); 
+    System.out.println("Please enter your final number"); 
+    int thirdNumber = input.nextInt();  
+    
+    //determine which number is maximum 
+    if(number >= secondNumber && number >= thirdNumber){
+      System.out.println("Maximum:" + number); 
+      
+    if(thirdNumber >= number && thirdNumber >= secondNumber); 
+    }else{
+      System.out.println("Maximum:" + secondNumber); 
+    }
+  
+  
+  
+   
+  
+
     
   }
 }
